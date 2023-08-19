1d930480fc3317dd462f17933694673ff9ef7d19
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-08-19 11:40:26 -0300

    feat:template

--- apps/ng-template-khan-esdras-portal/src/app/app.component.ts
+++ apps/ng-template-khan-esdras-portal/src/app/app.component.ts
@@ -3,25 +3,12 @@ import { CommonModule } from '@angular/common';
 import { BaseUrl } from '@ng-template-khan-esdras/shared/utils';
 import { RouterOutlet } from '@angular/router';
 
-
 @Component({
   selector: 'ng-template-khan-esdras-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: `
-     TeSAD ASDSAD
-  `,
-  styles: [
-    `
-      .pt-4rem {
-        padding-top: 4rem;
-      }
-
-      .pl-10rem {
-        padding: 1rem 1rem 1rem 11rem;
-      }
-    `,
-  ],
+  template: ` TeSAD ASDSAD `,
+  styles: [``],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
